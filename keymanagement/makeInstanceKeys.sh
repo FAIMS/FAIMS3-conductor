b5c23c4e1f9d0d7146f6dc89543d4f85f3741837
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-01-27 13:48:22 +1100

    move key script out of dockerfile
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- keymanagement/makeInstanceKeys.sh
+++ keymanagement/makeInstanceKeys.sh
@@ -2,16 +2,20 @@
 
 set -euo pipefail
 
-# TARGET_DIR="/etc/keys"
-# if [ $# -eq 1 ]; then
-# 	TARGET_DIR="$1"
-# fi
 
-mkdir -p /keys
-echo date > /keys/start
+if [ $# -eq 1 ]; then
+	HOST_TARGET="$1"
+else;
+	echo "./makeTestKeys.sh host"
+	exit 0;
+fi
+
+mkdir -p keys
+cd keys
+echo date > ${HOST_TARGET}start
 rm -f *.pem *.flattened
-openssl genpkey -algorithm RSA -out "private_key.pem" -pkeyopt rsa_keygen_bits:2048
-openssl rsa -pubout -in "private_key.pem" -out "public_key.pem"
+openssl genpkey -algorithm RSA -out "${HOST_TARGET}_private_key.pem" -pkeyopt rsa_keygen_bits:2048
+openssl rsa -pubout -in "${HOST_TARGET}_private_key.pem" -out "${HOST_TARGET}_public_key.pem"
 
 
 # #" Generate a private key (prime256v1 is the name of the parameters used
@@ -21,10 +25,10 @@ openssl rsa -pubout -in "private_key.pem" -out "public_key.pem"
 # openssl ec -in "$TARGET_DIR/ecdsa_p256v1_private_key.pem" -pubout -out "$TARGET_DIR/ecdsa_p256v1_public_key.pem"
 
 
-cat "public_key.pem" | sed ':a;N;$!ba;s/\n/\\n/g' > "rsa_2048_public_key.pem.flattened"
-cp *.pem /keys/
-cp *.flattened /keys/
-ls /keys/
+cat "${HOST_TARGET}_public_key.pem" | sed ':a;N;$!ba;s/\n/\\n/g' > "${HOST_TARGET}_rsa_2048_public_key.pem.flattened"
+# cp *.pem /keys/
+# cp *.flattened /keys/
+# ls /keys/
 #cat "$TARGET_DIR/ecdsa_p256v1_public_key.pem" | sed ':a;N;$!ba;s/\n/\\n/g' > "$TARGET_DIR/ecdsa_p256v1_public_key.pem.flattened"
 
-ls > /keys/list
+# ls > /keys/list
