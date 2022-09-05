d8bfab91ab9b788246d6227522f720a4f4fd1b72
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-09-05 17:04:40 +1000

    allowing for flattened keys for easy import into github JWT stuff
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- keymanagement/makeInstanceKeys.sh
+++ keymanagement/makeInstanceKeys.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+## no need for this to vary as in this deployment there is only ever one conductor-couchdb pair 
+## Putting this before the local .env to allow for multi-instance deploys.
+export HOST_TARGET='conductor'
+
 # Local .env
 if [ -f .env ]; then
     # Load Environment Variables
@@ -9,12 +13,14 @@ if [ -f .env ]; then
 fi
 
 
-## no need for this to vary as in this deployment there is only ever one conductor-couchdb pair
-export HOST_TARGET='conductor'
 
 mkdir -p keys
 openssl genpkey -algorithm RSA -out "keys/${HOST_TARGET}_private_key.pem" -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in "keys/${HOST_TARGET}_private_key.pem" -out "keys/${HOST_TARGET}_public_key.pem"
+
+## Dropping a flattened key out to support generated pubkeys for iOS testing
+
+cat "keys/${HOST_TARGET}_public_key.pem" | tr '\n' '\\n' > "keys/${HOST_TARGET}_rsa_2048_public_key.pem.flattened"
 
 ## Generate the jwt.ini file needed for couch deployment, contains the public key
 ## used to validate signed JWTs for authentication to couchdb
