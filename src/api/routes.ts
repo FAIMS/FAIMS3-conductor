550727a93f75f18fff2dbaed866f022b771a1454
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-03-16 10:16:35 +1100

    Unprotect initialisation API since it is a one-shot call
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -36,6 +36,7 @@ import {
   removeProjectRoleFromUser,
   saveUser,
   userHasPermission,
+  userIsClusterAdmin,
 } from '../couchdb/users';
 
 export const api = express.Router();
@@ -46,9 +47,11 @@ api.get('/hello/', requireAuthenticationAPI, (_req: any, res: any) => {
 
 /**
  * POST to /api/initialise does initialisation on the databases
- * - not sure how we'll use this or what protection it needs
+ * - this does not have any auth requirement because it should be used
+ *   to set up the users database and create the admin user
+ *   if databases exist, this is a no-op
  */
-api.post('/initialise/', requireAuthenticationAPI, async (req, res) => {
+api.post('/initialise/', async (req, res) => {
   initialiseDatabases();
   res.json({success: true});
 });
@@ -68,16 +71,25 @@ api.get('/notebooks/', requireAuthenticationAPI, async (req, res) => {
  */
 api.post('/notebooks/', requireAuthenticationAPI, async (req, res) => {
   // post a new notebook
-  const uiSpec = req.body['ui-specification'];
-  const projectName = req.body.name;
-  const metadata = req.body.metadata;
+  // user must be cluster admin
 
-  try {
-    const projectID = await createNotebook(projectName, uiSpec, metadata);
-    res.json({notebook: projectID});
-  } catch {
-    res.json({error: 'there was an error creating the notebook'});
-    res.status(500).end();
+  if (userIsClusterAdmin(user)) {
+    const uiSpec = req.body['ui-specification'];
+    const projectName = req.body.name;
+    const metadata = req.body.metadata;
+
+    try {
+      const projectID = await createNotebook(projectName, uiSpec, metadata);
+      res.json({notebook: projectID});
+    } catch {
+      res.json({error: 'there was an error creating the notebook'});
+      res.status(500).end();
+    }
+  } else {
+    res.json({
+      error: 'you do not have permission to create notebooks on this server',
+    });
+    res.status(401).end();
   }
 });
 
@@ -118,12 +130,21 @@ api.get(
 );
 
 api.get('/notebooks/:id/users/', requireAuthenticationAPI, async (req, res) => {
-  const userInfo = await getUserInfoForNotebook(req.params.id);
-  res.json(userInfo);
+  // user must have admin access tot his notebook
+
+  if (userHasPermission(req.user, req.params.id, 'modify')) {
+    const userInfo = await getUserInfoForNotebook(req.params.id);
+    res.json(userInfo);
+  } else {
+    res.json({
+      error: 'you do not have permission to view users for this notebook',
+    });
+    res.status(401).end();
+  }
 });
 
 // POST to give a user permissions on this notebook
-// body includes: 
+// body includes:
 //   {
 //     username: 'a username or email',
 //     role: a valid role for this notebook,
@@ -133,37 +154,44 @@ api.post(
   '/notebooks/:id/users/',
   requireAuthenticationAPI,
   async (req, res) => {
-    let error = '';
-    const notebook = await getNotebookMetadata(req.params.id);
-    if (notebook) {
-      const username = req.body.username;
-      const role = req.body.role;
-      const addrole = req.body.addrole;
 
-      // check that this is a legitimate role for this notebook
-      const notebookRoles = await getRolesForNotebook(notebook.project_id);
-      if (notebookRoles.indexOf(role) >= 0) {
-        const user = await getUserFromEmailOrUsername(username);
-        if (user) {
-          if (addrole) {
-            await addProjectRoleToUser(user, notebook.project_id, role);
+    if (userHasPermission(req.user, req.params.id, 'modify')) {
+      let error = '';
+      const notebook = await getNotebookMetadata(req.params.id);
+      if (notebook) {
+        const username = req.body.username;
+        const role = req.body.role;
+        const addrole = req.body.addrole;
+
+        // check that this is a legitimate role for this notebook
+        const notebookRoles = await getRolesForNotebook(notebook.project_id);
+        if (notebookRoles.indexOf(role) >= 0) {
+          const user = await getUserFromEmailOrUsername(username);
+          if (user) {
+            if (addrole) {
+              await addProjectRoleToUser(user, notebook.project_id, role);
+            } else {
+              await removeProjectRoleFromUser(user, notebook.project_id, role);
+            }
+            await saveUser(user);
+            res.json({status: 'success'});
+            return;
           } else {
-            await removeProjectRoleFromUser(user, notebook.project_id, role);
+            error = 'Unknown user ' + username;
           }
-          await saveUser(user);
-          res.json({status: 'success'});
-          return;
         } else {
-          error = 'Unknown user ' + username;
+          error = 'Unknow role';
         }
       } else {
-        error = 'Unknow role';
+        error = 'Unknown notebook';
       }
+      // user or project not found or bad role
+      res.json({status: 'error', error});
+      res.status(404).end();
     } else {
-      error = 'Unknown notebook';
+      res.json({
+        error: 'you do not have permission to modify users for this notebook',
+      });
+      res.status(401).end();
     }
-    // user or project not found or bad role
-    res.json({status: 'error', error});
-    res.status(404).end();
-  }
 );
